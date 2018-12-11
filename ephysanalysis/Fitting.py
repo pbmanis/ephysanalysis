10b7b3a9949ac29752dfa0cc1b2ae736af9c860f
Paul B. Manis <pmanis@med.unc.edu>
2018-12-11 15:48:09 -0500

    just some cleanup

--- ephysanalysis/Fitting.py
+++ ephysanalysis/Fitting.py
@@ -879,184 +879,186 @@ p[4]*np.exp(-(p[5] + x)/p[6]))**2.0
 # run tests if we are "main"
 
 if __name__ == "__main__":
+    pass
 #    import matplotlib.pyplot as pyplot
-    import timeit
-    import Fitting
-    import matplotlib as MP
-    MP.use('Qt4Agg')
-    ################## Do not modify the following code 
-    # sets up matplotlib with sans-serif plotting... 
-    import matplotlib.gridspec as GS
-    # import mpl_toolkits.axes_grid1.inset_locator as INSETS
-    # #import inset_axes, zoomed_inset_axes
-    # import mpl_toolkits.axes_grid1.anchored_artists as ANCHOR
-    # # import AnchoredSizeBar
+#     import timeit
+#     import Fitting
+#     import matplotlib as MP
+#     MP.use('Qt4Agg')
+#     ################## Do not modify the following code
+#     # sets up matplotlib with sans-serif plotting...
+#     import matplotlib.gridspec as GS
+#     # import mpl_toolkits.axes_grid1.inset_locator as INSETS
+#     # #import inset_axes, zoomed_inset_axes
+#     # import mpl_toolkits.axes_grid1.anchored_artists as ANCHOR
+#     # # import AnchoredSizeBar
+#
+#     stdFont = 'Arial'
+#
+#     import  matplotlib.pyplot as pylab
+#     pylab.rcParams['text.usetex'] = True
+#     pylab.rcParams['interactive'] = False
+#     pylab.rcParams['font.family'] = 'sans-serif'
+#     pylab.rcParams['font.sans-serif'] = 'Arial'
+#     pylab.rcParams['mathtext.default'] = 'sf'
+#     pylab.rcParams['figure.facecolor'] = 'white'
+#     # next setting allows pdf font to be readable in Adobe Illustrator
+#     pylab.rcParams['pdf.fonttype'] = 42
+#     ##################### to here (matplotlib stuff - touchy!
+#
+#     Fits = Fitting.Fitting()
+# #    x = np.arange(0, 100.0, 0.1)
+# #    y = 5.0-2.5*np.exp(-x/5.0)+0.5*np.random.randn(len(x))
+# #    (dc, aFit,tauFit) = Fits.expfit(x,y)
+# #    yf = dc + aFit*np.exp(-x/tauFit)
+#  #   pyplot.figure(1)
+#   #  pyplot.plot(x,y,'k')
+#   #  pyplot.plot(x, yf, 'r')
+#   #  pyplot.show()
+#     exploreError = False
+#
+#     if exploreError is True:
+#         # explore the error surface for a function:
+#
+#         func = 'exp1'
+#         f = Fits.fitfuncmap[func]
+#         p1range = np.arange(0.1, 5.0, 0.1)
+#         p2range = np.arange(0.1, 5.0, 0.1)
+#
+#         err = np.zeros((len(p1range), len(p2range)))
+#         x = np.array(np.arange(f[4][0], f[4][1], f[4][2]))
+#         C = None
+#         if func == 'expsum2':
+#           C = f[7]
+#
+#
+#         # check exchange of tau1 ([1]) and width[4]
+#         C = None
+#         yOffset, t0, tau1, tau2, amp, width = f[1] # get inital parameters
+#         y0 = f[0](f[1], x, C=C)
+#         noise = np.random.random(y0.shape) - 0.5
+#         y0 += 0.0* noise
+#         sh = err.shape
+#         yp = np.zeros((sh[0], sh[1], len(y0)))
+#         for i, p1 in enumerate(p1range):
+#             tau1t = tau1*p1
+#             for j, p2 in enumerate(p2range):
+#                 ampt = amp*p2
+#                 pars = (yOffset, t0, tau1t, tau2, ampt, width) # repackage
+#                 err[i,j] = f[0](pars, x, y0, C=C, sumsq = True)
+#                 yp[i,j] = f[0](pars, x, C=C, sumsq = False)
+#
+#         pylab.figure()
+#         CS=pylab.contour(p1range*tau1, p2range*width, err, 25)
+#         CB = pylab.colorbar(CS, shrink=0.8, extend='both')
+#         pylab.figure()
+#         for i, p1 in enumerate(p1range):
+#             for j, p2 in enumerate(p2range):
+#                 pylab.plot(x, yp[i,j])
+#         pylab.plot(x, y0, 'r-', linewidth=2.0)
+#
+#
+#     # run tests for each type of fit, return results to compare parameters
+#
+#     cons = None
+#     bnds = None
+#
+#     signal_to_noise = 100000.
+#     for func in Fits.fitfuncmap:
+#         if func != 'piecewiselinear3':
+#             continue
+#         print ("\nFunction: %s\nTarget: " % (func),)
+#         f = Fits.fitfuncmap[func]
+#         for k in range(0,len(f[1])):
+#             print ("%f " % (f[1][k]),)
+#         print ("\nStarting:     ",)
+#         for k in range(0,len(f[5])):
+#             print ("%f " % (f[5][k]),)
+#
+# #        nstep = 500.0
+# #        if func == 'sin':
+# #            nstep = 100.0
+#         x = np.arange(f[4][0], f[4][1], f[4][2])
+#         print('f4: ', f[4])
+#         print('x', x)
+#         C = None
+#         if func == 'expsum2':
+#             C = f[7]
+#
+#         if func == 'exppulse':
+#             C = f[7]
+#         tv = f[5]
+#         y = f[0](f[1], x, C=C)
+#         print(x)
+#         yd = np.array(y)
+#         noise = np.random.normal(0, 0.1, yd.shape)
+#         print(yd)
+#         my = np.amax(yd)
+#         #yd = yd + sigmax*0.05*my*(np.random.random_sample(shape(yd))-0.5)
+#         yd += noise*my/signal_to_noise
+#         testMethod = 'SLSQP'
+#         if func == 'taucurve':
+#             continue
+#             bounds=[(0., 100.), (0., 1000.), (0.0, 500.0), (0.1, 50.0),
+#                 (0., 1000), (0.0, 500.0), (0.1, 50.0)]
+#             (fpar, xf, yf, names) = Fits.FitRegion(np.array([1]), 0, x, yd, fitFunc = func, bounds=bounds, method=testMethod)
+#         elif func == 'boltz':
+#             continue
+#             bounds = [(-0.5,0.5), (0.0, 20.0), (-120., 0.), (-20., 0.)]
+#             (fpar, xf, yf, names) = Fits.FitRegion(np.array([1]), 0, x, yd, fitFunc = func, bounds=bounds, method=testMethod)
+#
+#         elif func == 'exp2':
+#             bounds=[(-0.001, 0.001), (-5.0, 0.), (1.0, 500.0), (-5.0, 0.0),
+#                 (1., 10000.)]
+#             (fpar, xf, yf, names) = Fits.FitRegion(np.array([1]), 0, x, yd, fitFunc = func, bounds=bounds, method=testMethod)
+#
+#         elif func == 'exppulse':
+#             # set some constraints to the fitting
+#             # yOffset, tau1, tau2, amp, width = f[1]  # order of constraings
+#             dt = np.mean(np.diff(x))
+#             bounds = [(-5, 5), (-15., 15.), (-2, 2.0), (2-10, 10.), (-5, 5.), (0., 5.)]
+#             # cxample for constraints:
+#             # cons = ({'type': 'ineq', 'fun': lambda x:   x[4] - 3.0*x[2]},
+#             #         {'type': 'ineq', 'fun': lambda x:   - x[4] + 12*x[2]},
+#             #         {'type': 'ineq', 'fun': lambda x:   x[2]},
+#             #         {'type': 'ineq', 'fun': lambda x:  - x[4] + 2000},
+#             #         )
+#             cons = ({'type': 'ineq', 'fun': lambda x:   x[3] - x[2] }, # tau1 < tau2
+#                 )
+#             C = None
+#
+#             tv = f[5]
+#             initialgr = f[0](f[5], x, None )
+#             (fpar, xf, yf, names) = Fits.FitRegion(
+#                 np.array([1]), 0, x, yd, fitFunc = func, fixedPars = C, constraints = cons, bounds = bounds, method=testMethod)
+#             # print xf
+#             # print yf
+#             # print fpar
+#             # print names
+#
+#         else:
+#             initialgr = f[0](f[5], x, None )
+#             (fpar, xf, yf, names) = Fits.FitRegion(
+#                 np.array([1]), 0, x, yd, fitFunc = func, fixedPars = C, constraints = cons, bounds = bnds, method=testMethod)
+#         #print fpar
+#         s = np.shape(fpar)
+#         j = 0
+#         outstr = ""
+#         initstr = ""
+#         truestr = ""
+#         for i in range(0, len(names[j])):
+# #            print "%f " % fpar[j][i],
+#             outstr = outstr + ('%s = %f, ' % (names[j][i], fpar[j][i]))
+#             initstr = initstr + '%s = %f, ' % (names[j][i], tv[i])
+#             truestr = truestr + '%s = %f, ' % (names[j][i], f[1][i])
+#         print( "\nTrue(%d) : %s" % (j, truestr) )
+#         print( "FIT(%d)   : %s" % (j, outstr) )
+#         print( "init(%d) : %s" % (j, initstr) )
+#         print( "Error:   : %f" % (Fits.fitSum2Err))
+#         if func is 'piecewiselinear3':
+#             pylab.figure()
+#             pylab.plot(np.array(x), yd, 'ro-')
+#             pylab.plot(np.array(x), initialgr, 'k--')
+#             pylab.plot(xf[0], yf[0], 'b-') # fit
+#             pylab.show()
 
-    stdFont = 'Arial'
-
-    import  matplotlib.pyplot as pylab
-    pylab.rcParams['text.usetex'] = True
-    pylab.rcParams['interactive'] = False
-    pylab.rcParams['font.family'] = 'sans-serif'
-    pylab.rcParams['font.sans-serif'] = 'Arial'
-    pylab.rcParams['mathtext.default'] = 'sf'
-    pylab.rcParams['figure.facecolor'] = 'white'
-    # next setting allows pdf font to be readable in Adobe Illustrator
-    pylab.rcParams['pdf.fonttype'] = 42
-    ##################### to here (matplotlib stuff - touchy!
-    
-    Fits = Fitting.Fitting()
-#    x = np.arange(0, 100.0, 0.1)
-#    y = 5.0-2.5*np.exp(-x/5.0)+0.5*np.random.randn(len(x))
-#    (dc, aFit,tauFit) = Fits.expfit(x,y)
-#    yf = dc + aFit*np.exp(-x/tauFit)
- #   pyplot.figure(1)
-  #  pyplot.plot(x,y,'k')
-  #  pyplot.plot(x, yf, 'r')
-  #  pyplot.show()
-    exploreError = False
-
-    if exploreError is True:
-        # explore the error surface for a function:
-
-        func = 'exp1'
-        f = Fits.fitfuncmap[func]
-        p1range = np.arange(0.1, 5.0, 0.1)
-        p2range = np.arange(0.1, 5.0, 0.1)
-
-        err = np.zeros((len(p1range), len(p2range)))
-        x = np.array(np.arange(f[4][0], f[4][1], f[4][2]))
-        C = None
-        if func == 'expsum2':
-          C = f[7]
-
-     
-        # check exchange of tau1 ([1]) and width[4]
-        C = None
-        yOffset, t0, tau1, tau2, amp, width = f[1] # get inital parameters
-        y0 = f[0](f[1], x, C=C)
-        noise = np.random.random(y0.shape) - 0.5
-        y0 += 0.0* noise
-        sh = err.shape
-        yp = np.zeros((sh[0], sh[1], len(y0)))
-        for i, p1 in enumerate(p1range):
-            tau1t = tau1*p1
-            for j, p2 in enumerate(p2range):
-                ampt = amp*p2
-                pars = (yOffset, t0, tau1t, tau2, ampt, width) # repackage
-                err[i,j] = f[0](pars, x, y0, C=C, sumsq = True)
-                yp[i,j] = f[0](pars, x, C=C, sumsq = False)
-  
-        pylab.figure()
-        CS=pylab.contour(p1range*tau1, p2range*width, err, 25)
-        CB = pylab.colorbar(CS, shrink=0.8, extend='both')
-        pylab.figure()
-        for i, p1 in enumerate(p1range):
-            for j, p2 in enumerate(p2range):
-                pylab.plot(x, yp[i,j])
-        pylab.plot(x, y0, 'r-', linewidth=2.0)
-
-  
-    # run tests for each type of fit, return results to compare parameters
-
-    cons = None
-    bnds = None
-    
-    signal_to_noise = 100000.
-    for func in Fits.fitfuncmap:
-        if func != 'piecewiselinear3':
-            continue
-        print ("\nFunction: %s\nTarget: " % (func),)
-        f = Fits.fitfuncmap[func]
-        for k in range(0,len(f[1])):
-            print ("%f " % (f[1][k]),)
-        print ("\nStarting:     ",)
-        for k in range(0,len(f[5])):
-            print ("%f " % (f[5][k]),)
-
-#        nstep = 500.0
-#        if func == 'sin':
-#            nstep = 100.0
-        x = np.arange(f[4][0], f[4][1], f[4][2])
-        print('f4: ', f[4])
-        print('x', x)
-        C = None
-        if func == 'expsum2':
-            C = f[7]
-
-        if func == 'exppulse':
-            C = f[7]
-        tv = f[5]
-        y = f[0](f[1], x, C=C)
-        print(x)
-        yd = np.array(y)
-        noise = np.random.normal(0, 0.1, yd.shape)
-        print(yd)
-        my = np.amax(yd)
-        #yd = yd + sigmax*0.05*my*(np.random.random_sample(shape(yd))-0.5)
-        yd += noise*my/signal_to_noise
-        testMethod = 'SLSQP'
-        if func == 'taucurve':
-            continue
-            bounds=[(0., 100.), (0., 1000.), (0.0, 500.0), (0.1, 50.0), 
-                (0., 1000), (0.0, 500.0), (0.1, 50.0)]
-            (fpar, xf, yf, names) = Fits.FitRegion(np.array([1]), 0, x, yd, fitFunc = func, bounds=bounds, method=testMethod)
-        elif func == 'boltz':
-            continue
-            bounds = [(-0.5,0.5), (0.0, 20.0), (-120., 0.), (-20., 0.)]
-            (fpar, xf, yf, names) = Fits.FitRegion(np.array([1]), 0, x, yd, fitFunc = func, bounds=bounds, method=testMethod)
-        
-        elif func == 'exp2':
-            bounds=[(-0.001, 0.001), (-5.0, 0.), (1.0, 500.0), (-5.0, 0.0), 
-                (1., 10000.)]
-            (fpar, xf, yf, names) = Fits.FitRegion(np.array([1]), 0, x, yd, fitFunc = func, bounds=bounds, method=testMethod)
-        
-        elif func == 'exppulse':
-            # set some constraints to the fitting
-            # yOffset, tau1, tau2, amp, width = f[1]  # order of constraings
-            dt = np.mean(np.diff(x))
-            bounds = [(-5, 5), (-15., 15.), (-2, 2.0), (2-10, 10.), (-5, 5.), (0., 5.)]
-            # cxample for constraints:
-            # cons = ({'type': 'ineq', 'fun': lambda x:   x[4] - 3.0*x[2]},
-            #         {'type': 'ineq', 'fun': lambda x:   - x[4] + 12*x[2]},
-            #         {'type': 'ineq', 'fun': lambda x:   x[2]},
-            #         {'type': 'ineq', 'fun': lambda x:  - x[4] + 2000},
-            #         )
-            cons = ({'type': 'ineq', 'fun': lambda x:   x[3] - x[2] }, # tau1 < tau2 
-                )
-            C = None
-
-            tv = f[5]
-            initialgr = f[0](f[5], x, None )
-            (fpar, xf, yf, names) = Fits.FitRegion(
-                np.array([1]), 0, x, yd, fitFunc = func, fixedPars = C, constraints = cons, bounds = bounds, method=testMethod)
-            # print xf
-            # print yf
-            # print fpar
-            # print names
-        
-        else:
-            initialgr = f[0](f[5], x, None )
-            (fpar, xf, yf, names) = Fits.FitRegion(
-                np.array([1]), 0, x, yd, fitFunc = func, fixedPars = C, constraints = cons, bounds = bnds, method=testMethod)
-        #print fpar
-        s = np.shape(fpar)
-        j = 0
-        outstr = ""
-        initstr = ""
-        truestr = ""
-        for i in range(0, len(names[j])):
-#            print "%f " % fpar[j][i],
-            outstr = outstr + ('%s = %f, ' % (names[j][i], fpar[j][i]))
-            initstr = initstr + '%s = %f, ' % (names[j][i], tv[i])
-            truestr = truestr + '%s = %f, ' % (names[j][i], f[1][i])
-        print( "\nTrue(%d) : %s" % (j, truestr) )
-        print( "FIT(%d)   : %s" % (j, outstr) )
-        print( "init(%d) : %s" % (j, initstr) )
-        print( "Error:   : %f" % (Fits.fitSum2Err))
-        if func is 'piecewiselinear3':
-            pylab.figure()
-            pylab.plot(np.array(x), yd, 'ro-')
-            pylab.plot(np.array(x), initialgr, 'k--')
-            pylab.plot(xf[0], yf[0], 'b-') # fit
-            pylab.show()
