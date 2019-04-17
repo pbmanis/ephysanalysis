2c7c86bb7682f75b222ebbec66f81ee26bd3f172
Paul B. Manis <pmanis@med.unc.edu>
2019-04-16 21:40:37 -0400

    add parameterTree and control over marking

--- ephysanalysis/cursor_plot.py
+++ ephysanalysis/cursor_plot.py
@@ -2,27 +2,108 @@ import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
 from pyqtgraph.Point import Point
+from pyqtgraph.parametertree import Parameter, ParameterTree
+font = QtGui.QFont()
+font.setFamily('Arial')
+font.setFixedPitch(False)
+font.setPointSize(11)
+
 
 #generate layout
 class CursorPlot(object):
     def __init__(self):
+        
+
+        self.nextStore = 'T0'
         self.app = QtGui.QApplication([])
         self.win = pg.GraphicsWindow()
+        self.layout = QtGui.QGridLayout()
+        self.win.setLayout(self.layout)
+        self.win.resize(1024,800)
+        
         self.win.setWindowTitle('pyqtgraph example: crosshair')
         self.label = pg.LabelItem(justify='right')
         self.win.addItem(self.label)
-        self.p1 = self.win.addPlot(row=1, col=0)
-        self.p2 = self.win.addPlot(row=2, col=0)
+        # self.plots['Windowed'] = self.win.addPlot(row=1, col=0)
+        # self.plots['Full'] = self.win.addPlot(row=2, col=0)
+
+        params = [
+                {'name': 'Cursor Plot', 'type': 'group', 'children': [
+                    {'name': 'Save T0', 'type': 'action'},
+                    {'name': 'Save T1', 'type': 'action'},
+                    {'name': 'Done', 'type': 'action'},
+                ],
+                },
+                {'name': 'Data', 'type': 'group', 'children': [
+                    {'name': 'NextStore', 'type': 'str', 'value': self.nextStore},
+                    {'name': 'T0', 'type': 'float', 'value': 0.},
+                    {'name': 'T1', 'type': 'float', 'value': 0.},
+                ],
+                }
+            
+            ]
+        ptree = ParameterTree()
+        ptree.setMaximumWidth(150)
+        ptree.setFont(font)
+        ptree.sizeHintForRow(24)
+#        ptree.setUniformRowHeights(True)
+        self.ptreedata = Parameter.create(name='params', type='group', children=params)
+        ptree.setParameters(self.ptreedata, showTop=False)
+        print(self.ptreedata.param)
+        self.T0 = self.ptreedata.param('Data').param('T0')
+        print(self.T0.value()) # 'T0')
+        self.T1 = self.ptreedata.param('Data').param('T1')
+        self.T0.sigValueChanged.connect(self.T0_Changed)
+        self.T1.sigValueChanged.connect(self.T1_Changed)
+        
+
+
+        self.layout.addWidget(ptree, 0, 0, 9, 1) # Parameter Tree on left
+
+        # add space for the graphs
+        view = pg.GraphicsView()
+        layout = pg.GraphicsLayout(border=(50,100,50))
+        view.setCentralItem(layout)
+        self.layout.addWidget(view, 1, 1, 8, 1)  # data plots on right
+
+        self.plots = {}
+        self.plots['Windowed'] = layout.addPlot(title="Windowed")
+        layout.nextRow()
+        self.plots['Full'] = layout.addPlot(title="Full")
+
+        self.ptreedata.sigTreeStateChanged.connect(self.command_dispatcher)  # connect parameters to their updates
 
         self.region = pg.LinearRegionItem()
         self.region.setZValue(10)
         # Add the LinearRegionItem to the ViewBox, but tell the ViewBox to exclude this 
         # item when doing auto-range calculations.
-        self.p2.addItem(self.region, ignoreBounds=True)
+        self.plots['Full'].addItem(self.region, ignoreBounds=True)
 
         #pg.dbg()
-        self.p1.setAutoVisible(y=True)
-        self.proxy = pg.SignalProxy(self.p1.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
+        self.plots['Windowed'].setAutoVisible(y=True)
+        self.proxy = pg.SignalProxy(self.plots['Windowed'].scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
+        self.proxyclick = pg.SignalProxy(self.plots['Windowed'].scene().sigMouseClicked, rateLimit=60, slot=self.mouseClicked)
+    
+    def command_dispatcher(self, param, changes):
+        for param, change, data in changes:
+            path = self.ptreedata.childPath(param)
+            if path[1] == 'Done':
+                # close window
+                pass
+            elif path[1] == 'Save T0':
+                self.nextstore = 'T0'
+            elif path[1] == 'Save T1':
+                self.nextstore = 'T1'
+            
+            
+            pass
+            
+    def T0_Changed(self):
+        pass
+    
+    def T1_Changed(self):
+        pass
+    
 
     def make_testdata(self): #create numpy arrays
         #make the numbers large to show that the xrange shows data from 10000 to all the way 0
@@ -34,13 +115,13 @@ class CursorPlot(object):
     def plotData(self, x, y, setline=True):
         self.dataX = x
         self.dataY = y
-        self.p1.plot(x=self.dataX, y=self.dataY, pen="g")
-        # self.p1.plot(self.dataY, pen="g")
-        self.p2.plot(x=self.dataX, y=self.dataY, pen="w")
+        self.plots['Windowed'].plot(x=self.dataX, y=self.dataY, pen="g")
+        # self.plots['Windowed'].plot(self.dataY, pen="g")
+        self.plots['Full'].plot(x=self.dataX, y=self.dataY, pen="w")
         
         if setline:
             self.region.sigRegionChanged.connect(self.update)
-            self.p1.sigRangeChanged.connect(self.updateRegion)
+            self.plots['Windowed'].sigRangeChanged.connect(self.updateRegion)
             xmin = np.min(self.dataX)
             xmax = np.max(self.dataX)
             xmin = 0.1*(xmax-xmin) + xmin
@@ -50,14 +131,14 @@ class CursorPlot(object):
             #cross hair
             self.vLine = pg.InfiniteLine(angle=90, movable=False)
             self.hLine = pg.InfiniteLine(angle=0, movable=False)
-            self.p1.addItem(self.vLine, ignoreBounds=True)
-            self.p1.addItem(self.hLine, ignoreBounds=True)
-            self.vb = self.p1.vb
+            self.plots['Windowed'].addItem(self.vLine, ignoreBounds=True)
+            self.plots['Windowed'].addItem(self.hLine, ignoreBounds=True)
+            self.vb = self.plots['Windowed'].vb
 
     def update(self):
         self.region.setZValue(10)
         minX, maxX = self.region.getRegion()
-        self.p1.setXRange(minX, maxX, padding=0)    
+        self.plots['Windowed'].setXRange(minX, maxX, padding=0)    
 
     def updateRegion(self, window, viewRange):
         rgn = viewRange[0]
@@ -65,21 +146,36 @@ class CursorPlot(object):
         self.rgn = rgn
 
     def mouseMoved(self, evt):
+        # evt is a PyQt5.QtCore.QPointF
         pos = evt[0]  ## using signal proxy turns original arguments into a tuple
-        if self.p1.sceneBoundingRect().contains(pos):
+        if self.plots['Windowed'].sceneBoundingRect().contains(pos):
             mousePoint = self.vb.mapSceneToView(pos)
             index = int(mousePoint.x())
             if index > 0 and index < len(self.dataX):
                 self.label.setText("<span style='font-size: 12pt'>x=%0.3f,   <span style='color: red'>y1=%0.1f</span>,   <span style='color: green'>y2=%0.1f</span>" % (mousePoint.x(), mousePoint.y(), self.dataY[index]))
             self.vLine.setPos(mousePoint.x())
             self.hLine.setPos(mousePoint.y())
-    #p1.scene().sigMouseMoved.connect(mouseMoved)
+            self.lastx = mousePoint.x()
+            self.lasty = mousePoint.y()
 
+    def mouseClicked(self, evt):
+        print(evt)
+        print(f"<{self.nextStore:s}>")
+        # print(dir(self.T0))
+        if self.nextStore == 'T0':
+            self.T0.setValue(self.lastx)
+            self.nextStore = 'T1'
+        elif self.nextStore == 'T1':
+            self.T1.setValue(self.lastx)
+            self.nextStore = 'T0'
+        else:
+            pass
+        
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
     import sys
-    CP = CursorPlotSetup()
+    CP = CursorPlot()
     x, y = CP.make_testdata()
     CP.plotData(x, y)
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
